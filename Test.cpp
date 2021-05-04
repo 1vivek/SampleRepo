420dae1282ed749453f52551d74b384421291477
Vivek Chauhan <vivekvc1.732@gmail.com>
2021-05-04 15:43:01 +0530

    branch 1 modifications

--- Test.cpp
+++ Test.cpp
@@ -4,10 +4,6 @@ using namesppace std;
 int main()
 {
 	cout<<"This is Test 1 in newgitdir";
-<<<<<<< HEAD
-//Now changed in master
-=======
-//Modifyinng in branch 1
->>>>>>> branch1
+        cout<<"Branch 1";
 	return 0;
 }
